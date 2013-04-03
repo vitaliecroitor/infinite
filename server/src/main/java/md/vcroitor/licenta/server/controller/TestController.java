7643d13b21282a13ba7a3de1733dcd7bf1f4b2a6
vitaliecroitor <engineer.vitalie.croitor@gmail.com>
2013-04-03 16:48:01 +0300

    mongo db setup

--- server/src/main/java/md/vcroitor/licenta/server/controller/TestController.java
+++ server/src/main/java/md/vcroitor/licenta/server/controller/TestController.java
@@ -1,17 +1,33 @@
 package md.vcroitor.licenta.server.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 public class TestController {
 
     private static final Logger log = Logger.getLogger(TestController.class);
+    @Resource(name = "mongoTemplate")
+    MongoOperations mongoOperation;
 
     @RequestMapping(value = "/index")
     public String test() {
-        log.info("test test");
+
+        User user = new User("vitalik", "password123");
+
+        // save
+        mongoOperation.save(user, "users");
+
+        // find
+        User savedUser = mongoOperation.findOne(new Query(Criteria.where("username").is("vitalik")), User.class, "users");
+
+        log.info("Got: " + savedUser.getUsername() + "    " + savedUser.getPassword());
         return "index";
     }
 }
