74c559c4f7f10ac717a9ffe99893ffdffd0d5b58
Vitalie Croitor <vcroitor@vcroitor.(none)>
2013-04-21 18:51:16 +0300

    added test

---
+++ server/src/test/java/md/vcroitor/licenta/server/controller/PromotionControllerTest.java
@@ -0,0 +1,11 @@
+package md.vcroitor.licenta.server.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vcroitor
+ * Date: 4/21/13
+ * Time: 5:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PromotionControllerTest {
+}
